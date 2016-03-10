ce51d3974e9571c51743f8f8afc5c2de19c68e7c
Mandar Madhukar Joshi <mandar.joshi2@mindtree.com>
2016-03-10 15:08:18 +0530

    Use env variables

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,24 +1,23 @@
 require 'selenium-webdriver'
 require 'capybara/cucumber'
 require 'rspec/expectations'
-require 'site_prism'
+#require 'site_prism'
 require 'capybara/poltergeist'
 require 'appium_capybara'
 
-
+DEVICE_ID = ENV['ADB_DEVICE_ARG']
+APPIUM_PORT = ENV['APPIUM_PORT']
 def appium_caps
   {
-    'MY_DEVICE'=> { platformName: "android", deviceName: '#{ENV['ADB_DEVICE_ARG']}', browserName: "chrome" } #versionNumber: "5.1.1", browserName: "chrome"}
+    'MY_DEVICE'=> { platformName: "android", deviceName: "#{DEVICE_ID}", browserName: "chrome" }
   }
 end
 
 
 Capybara.register_driver :appium do |app|
-  puts "in it"
   caps = appium_caps.fetch('MY_DEVICE')
-  puts "#{caps}"
   desired_caps = caps
-  url = "http://127.0.0.1:'#{ENV['APPIUM_PORT']}'/wd/hub/" # Url to your running appium server
+  url = "http://127.0.0.1:'#{APPIUM_PORT}'/wd/hub/" # Url to your running appium server
   appium_lib_options = { server_url: url }
   all_options = { appium_lib:  appium_lib_options, caps: desired_caps }
   Appium::Capybara::Driver.new app, all_options
