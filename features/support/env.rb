3be54dec90faaa7da9a2796dc88b00b7bd19142d
Mandar Madhukar Joshi <mandar.joshi2@mindtree.com>
2016-03-10 15:26:19 +0530

    Update env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -16,7 +16,7 @@ end
 Capybara.register_driver :appium do |app|
   caps = appium_caps.fetch('MY_DEVICE')
   desired_caps = caps
-  url = 'http://127.0.0.1:"#{APPIUM_PORT}"/wd/hub/' # Url to your running appium server
+  url = "http://127.0.0.1:#{APPIUM_PORT}/wd/hub/" # Url to your running appium server
   appium_lib_options = { server_url: url }
   all_options = { appium_lib:  appium_lib_options, caps: desired_caps }
   Appium::Capybara::Driver.new app, all_options
