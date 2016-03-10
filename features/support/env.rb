a92e23adae56f78d7f0032e832c0b4a16a575d6b
Mandar Madhukar Joshi <mandar.joshi2@mindtree.com>
2016-03-10 16:01:30 +0530

    Update env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -20,7 +20,8 @@ Capybara.register_driver :appium do |app|
   url = "http://127.0.0.1:#{APPIUM_PORT}/wd/hub/" # Url to your running appium server
   appium_lib_options = { server_url: url }
   all_options = { appium_lib:  appium_lib_options, caps: desired_caps }
-  Appium::Capybara::Driver.new app, all_options
+  Capybara::Selenium::Driver.new(app, {:browser => :remote, :url => url, :desired_capabilities => caps})
+  #Appium::Capybara::Driver.new app, all_options
 end
 
 Capybara.default_driver = :appium
