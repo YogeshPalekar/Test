3cc427be28149578b6bb2f0a4b2facdce88abfa4
Mandar Madhukar Joshi <mandar.joshi2@mindtree.com>
2016-03-10 14:48:58 +0530

    Modification to run on Hive

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,23 +4,21 @@ require 'rspec/expectations'
 require 'site_prism'
 require 'capybara/poltergeist'
 require 'appium_capybara'
-require 'pry'
 
 
 def appium_caps
   {
-    'Nexus5'=> { platformName: "android", deviceName: "Nexus 5", versionNumber: "5.1", browserName: "Chrome"},
-    'SGS 6'=> { platformName: "android", deviceName: "02157df23bc41912", versionNumber: "5.1.1", browserName: "chrome"}
+    'MY_DEVICE'=> { platformName: "android", deviceName: '#{ENV['ADB_DEVICE_ARG']}', browserName: "chrome" } #versionNumber: "5.1.1", browserName: "chrome"}
   }
 end
 
 
 Capybara.register_driver :appium do |app|
   puts "in it"
-  caps = appium_caps.fetch('SGS 6')
+  caps = appium_caps.fetch('MY_DEVICE')
   puts "#{caps}"
   desired_caps = caps
-  url = "http://127.0.0.1:4723/wd/hub/" # Url to your running appium server
+  url = "http://127.0.0.1:'#{ENV['APPIUM_PORT']}'/wd/hub/" # Url to your running appium server
   appium_lib_options = { server_url: url }
   all_options = { appium_lib:  appium_lib_options, caps: desired_caps }
   Appium::Capybara::Driver.new app, all_options
